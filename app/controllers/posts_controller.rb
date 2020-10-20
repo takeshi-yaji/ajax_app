58b96355b57be3b1394b7c852d4e3079a126cbbf
takeshi-yaji <72482665+takeshi-yaji@users.noreply.github.com>
2020-10-20 18:09:21 +0900

    コントローラー追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC") #全てのレコードを@postsに代入
+
   end
 
   def create
@@ -8,4 +9,15 @@ class PostsController < ApplicationController
     redirect_to action: :index
   end
 
+  def checked
+    post = Post.find(params[:id])
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
